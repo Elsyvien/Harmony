a766b8dc91cbff750bbc93a7b962c04a532ccbe5
Max <max@staneker.com>
2026-02-12 01:26:01 +0100

    Update main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { App } from './App';
 import { AuthProvider } from './store/auth-store';
 import './styles/global.css';
@@ -8,10 +8,10 @@ import './styles/user-sidebar.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
